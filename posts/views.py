4cb145651213bea672916caa5447b3d70eacc6ee
Urko Ruiz <zaszur@gmail.com>
2016-09-22 12:26:24 +0200

    Mejora en el listado de posts del inicio recuperando solo los ultimos 5 posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from posts.models import Post
 
-# Create your views here.
+
+def home(request):
+    """
+    Renderiza el home con un listado de fotos
+    :param request: objeto HttpRequest con los datos de la petición
+    :return: objeto HttpResposne con los datos de la respuesta
+    """
+    posts = Post.objects.all().order_by('-created_at')  # Recupera todos los posts de la base de datos
+    context = {'posts_list': posts[:4]}
+    return render(request, 'posts/home.html', context)
