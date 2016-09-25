a58b2fd9d28da164ad49a3d0117bf5e0c5d3b9db
Urko Ruiz <zaszur@gmail.com>
2016-09-25 09:14:25 +0200

    Refactorizacion de URLs

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from users.views import LoginView, LogoutView
+
+urlpatterns = [
+    url(r'^login$', LoginView.as_view(), name='users_login'),
+    url(r'^logout$', LogoutView.as_view(), name='users_logout'),
+]
--- wordplease/urls.py
+++ wordplease/urls.py
@@ -1,32 +1,12 @@
-"""wordplease URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from posts.views import HomeView, PostDetailView, PostCreationView, PostListView
-from users.views import LoginView, LogoutView
+from posts import urls as posts_urls
+from users import urls as users_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^login$', LoginView.as_view(), name='users_login'),
-    url(r'^logout$', LogoutView.as_view(), name='users_logout'),
-
-    url(r'^new_post$', PostCreationView.as_view(), name='posts_create'),
-    url(r'^posts/$', PostListView.as_view(), name='my_posts'),
-    url(r'^blogs/(?P<pk>\d+)$', PostDetailView.as_view(), name="posts_detail"),
-    url(r'^$', HomeView.as_view(), name='posts_home'),
+    url(r'', include(posts_urls)),
+    url(r'', include(users_urls)),
 ]
